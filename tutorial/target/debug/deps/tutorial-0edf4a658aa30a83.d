608487d35d8ef817bc782652c056654122a51fee
TurtlesMaster1 <165605491+TurtlesMaster1@users.noreply.github.com>
2025-08-02 16:15:54 +0800

    auto rust

---
+++ tutorial/target/debug/deps/tutorial-0edf4a658aa30a83.d
@@ -0,0 +1,5 @@
+C:\Users\Nathan-password is 1\Documents\game\tutorial\target\debug\deps\tutorial-0edf4a658aa30a83.d: src\main.rs
+
+C:\Users\Nathan-password is 1\Documents\game\tutorial\target\debug\deps\libtutorial-0edf4a658aa30a83.rmeta: src\main.rs
+
+src\main.rs:
---
+++ tutorial/target/debug/deps/tutorial-312dbfad11475e15.d
@@ -0,0 +1,5 @@
+C:\Users\Nathan-password is 1\Documents\game\tutorial\target\debug\deps\tutorial-312dbfad11475e15.d: src\main.rs
+
+C:\Users\Nathan-password is 1\Documents\game\tutorial\target\debug\deps\libtutorial-312dbfad11475e15.rmeta: src\main.rs
+
+src\main.rs:
